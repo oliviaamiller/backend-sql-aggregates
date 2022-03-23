fb29bb08edaf8efe697daaa07cca8870f836a89b
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-23 12:56:32 -0700

    exercise 4 passing

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,18 @@
 -- the average film length by category
 -- include the category name and avg length
+
+SELECT
+	name,
+    AVG(film.length)
+FROM
+	category
+INNER JOIN
+	film_category
+ON
+	film_category.category_id = category.category_id
+INNER JOIN
+	film
+ON 
+	film.film_id = film_category.film_id
+GROUP BY
+	category.name;
